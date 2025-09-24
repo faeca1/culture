3abc0f97f19c7ae856b107908945698fbd3170c3
faeca1 <faeca1.codes@gmail.com>
2025-09-24 11:09:02 +0100

    extract fake log to exposed function

--- packages/std/lib/log.js
+++ packages/std/lib/log.js
@@ -2,11 +2,21 @@ export function withChildLogger(obj, name) {
   return { ...obj, logger: child(obj.logger, name) };
 }
 
-
 export function child(logger, name) {
-  if (!logger) return { debug() { }, info() { }, warn() { }, error() { }, trace() { } };
+  if (!logger) return fake();
   if (!logger.child) return logger;
   return logger.child({ component: name });
 }
 
-
+export function fake() {
+  return {
+    child() {
+      return fake();
+    },
+    trace() {},
+    debug() {},
+    info() {},
+    warn() {},
+    error() {},
+  };
+}
