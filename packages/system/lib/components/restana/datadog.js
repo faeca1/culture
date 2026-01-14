1b1569599e774af17f0b42a44b058cc3e29ca878
faeca1 <faeca1.codes@gmail.com>
2026-01-14 12:21:14 +0000

    fix datadog

--- packages/system/lib/components/restana/datadog.js
+++ packages/system/lib/components/restana/datadog.js
@@ -1,8 +1,17 @@
 import { optional } from "../utils.js";
-const methods = ['get', 'delete', 'patch', 'post', 'put', 'head', 'options', 'trace', 'all'];
+const methods = [
+  "get",
+  "delete",
+  "patch",
+  "post",
+  "put",
+  "head",
+  "options",
+  "trace",
+  "all",
+];
 
 export default component;
-
 
 function component(opts = {}) {
   const agent = opts.dd || apm();
@@ -15,21 +24,21 @@ function component(opts = {}) {
       const ref = app[method];
 
       app[method] = (path, ...args) => {
-        args.unshift(agent.wrap("web.request", async (req, __, next) => {
+        args.unshift((req, __, next) => {
           try {
-            agent?.scope?.().active().setTag("resource.name", `${req.method} ${path}`);
-            return await next();
+            const name = `${req.method} ${path}`;
+            agent?.scope().active()?.setTag("resource.name", name);
+            return next();
           } catch (e) {
-            return await next(e);
+            return next(e);
           }
-        }));
+        });
 
         return ref(path, args);
       };
     });
   }
 }
-
 
 function apm() {
   const pkg = optional("dd-trace");
