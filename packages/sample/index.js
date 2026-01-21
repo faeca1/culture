b6cbec2b0c5c57d0fdcb55071a54c8304ae554ba
faeca1 <faeca1.codes@gmail.com>
2026-01-21 14:05:35 +0000

    update sample app

--- packages/sample/index.js
+++ packages/sample/index.js
@@ -1,11 +1,14 @@
 const _ = require("@faeca1/matter").default;
 const packages = { apiSchemaBuilder: require("api-schema-builder") };
 const docs = require("./spec");
+const System = require("./system");
 
 main();
 
 async function main() {
-  const system = _.system(definition(), { packages });
+  const handlers = _.web.handlers({ getPersons, getPersonById });
+  const definition = { ...System, docs, handlers };
+  const system = _.system(definition, { packages });
   const { logger } = await _.system.runner(system).start();
   logger.info("system started");
 }
@@ -22,30 +25,4 @@ function getPersonById(__, req) {
   const id = req.params.id;
   const person = { id, name: "pizza", age: 10 };
   return _.web.response.ok(person);
-}
-
-function definition() {
-  const config = {
-    http: {
-      auth: { basic: { allowed: "user:ssshhh:3" } },
-      routes: { roles: { ping: 0, read: 1 } },
-      swagger: { buildResponses: false },
-    },
-    logger: { level: "debug", name: "sample-app" },
-  };
-
-  const http = _.system.toDefinitions({
-    auth: ["auth", "app", "reqLog"],
-    app: ["app", "logger"],
-    datadog: ["datadog", "app"],
-    docs,
-    reqLog: ["reqLog", ["app", "logger"]],
-    routes: ["routes", ["app", "auth", "docs", "handlers"], ["datadog", "swagger"]],
-    server: ["server", ["app", "logger"], "routes"],
-    swagger: ["swagger", ["app", "docs"], "auth"],
-  });
-
-  const handlers = _.web.handlers({ getPersons, getPersonById });
-
-  return { config, http, logger: "bole", handlers };
 }
