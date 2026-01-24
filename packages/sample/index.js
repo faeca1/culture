e1e83b91ebbfab5559bead2ad8cb2d3dfe2aac27
faeca1 <faeca1.codes@gmail.com>
2026-01-24 21:57:10 +0000

    use new factory in sample app

--- packages/sample/index.js
+++ packages/sample/index.js
@@ -6,11 +6,14 @@ const System = require("./system");
 main();
 
 async function main() {
+  const factoryMethod = _.mm.method("foo", () => ({ fizz: "buzz" }));
+  const factory = _.mm.fromMulti(factoryMethod)(_.system.factory);
+
   const handlers = _.web.handlers({ getPersons, getPersonById });
   const definition = { ...System, docs, handlers };
-  const system = _.system(definition, { packages });
-  const { logger } = await _.system.runner(system).start();
-  logger.info("system started");
+  const system = _.system(definition, { factory, packages });
+  const { foo, logger } = await _.system.runner(system).start();
+  logger.info({ foo }, "system started");
 }
 
 function getPersons() {
